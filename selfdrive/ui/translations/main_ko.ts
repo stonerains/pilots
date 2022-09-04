5db66d7792b7f3040bcdce5ada9af0b9ed25699e
boxkon <ghkdxo14@gmail.com>
2022-09-05 06:38:43 +0900

    Update main_ko.ts

--- selfdrive/ui/translations/main_ko.ts
+++ selfdrive/ui/translations/main_ko.ts
@@ -176,6 +176,21 @@
         <source>Show Debug UI</source>
         <translation>디버그 메세지 보이기</translation>
     </message>
+    <message>
+        <location filename="../qt/offroad/settings.cc" line="692"/>
+        <source>Show Gear UI</source>
+        <translation>기어 단수 보이기</translation>
+    </message>
+    <message>
+        <location filename="../qt/offroad/settings.cc" line="698"/>
+        <source>Show EngRPM UI</source>
+        <translation>엔진 RPM 보이기</translation>
+    </message>
+    <message>
+        <location filename="../qt/offroad/settings.cc" line="704"/>
+        <source>Show Turnsignal UI</source>
+        <translation>방향지시신호 보이기</translation>
+    </message>
 </context>
 <context>
     <name>ConfirmationDialog</name>
