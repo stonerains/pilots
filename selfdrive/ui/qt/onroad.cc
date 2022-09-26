adf6c45a0343998e95984def6bb2def46105131a
boxkon <ghkdxo14@gmail.com>
2022-09-26 15:30:57 +0900

    update paint path

--- selfdrive/ui/qt/onroad.cc
+++ selfdrive/ui/qt/onroad.cc
@@ -337,7 +337,8 @@ void NvgWindow::drawLaneLines(QPainter &painter, const UIState *s) {
       bg.setColorAt(0.0, QColor::fromHslF(start_hue / 360., 0.97, 0.56, 0.4));
       bg.setColorAt(0.5, QColor::fromHslF(end_hue / 360., 1.0, 0.68, 0.35));
       bg.setColorAt(1.0, QColor::fromHslF(end_hue / 360., 1.0, 0.68, 0.0));
-    } else if (scene.end_to_end) {
+    }
+    else if (scene.end_to_end) {
       const auto &orientation = (*s->sm)["modelV2"].getModelV2().getOrientation();
       float orientation_future = 0;
       if (orientation.getZ().size() > 16) {
@@ -358,7 +359,8 @@ void NvgWindow::drawLaneLines(QPainter &painter, const UIState *s) {
       // The user is applying torque to the steering wheel
       bg.setColorAt(0, steeringpressedColor(200));
       bg.setColorAt(1, QColor(0, 95, 128, 50));
-    } else if (scene.override) {
+    }
+    else if (scene.override) {
       bg.setColorAt(0, overrideColor(200));
       bg.setColorAt(1, QColor(72, 77, 74, 50));
     }/*else {
@@ -366,9 +368,20 @@ void NvgWindow::drawLaneLines(QPainter &painter, const UIState *s) {
       bg.setColorAt(0.75 / 1.5, QColor::fromHslF(end_hue / 360., 1.0, 0.68, 0.35));
       bg.setColorAt(1.0, QColor::fromHslF(end_hue / 360., 1.0, 0.68, 0.0));
     }*/
-  } else {
-    bg.setColorAt(0, whiteColor(200));
-    bg.setColorAt(1, whiteColor(0));
+  }
+  else {
+    const auto &orientation = (*s->sm)["modelV2"].getModelV2().getOrientation();
+    float orientation_future = 0;
+    if (orientation.getZ().size() > 16) {
+      orientation_future = std::abs(orientation.getZ()[16]);  // 2.5 seconds
+    }
+    float curve_hue = fmax(0, 220 - (orientation_future * 800));
+    // FIXME: painter.drawPolygon can be slow if hue is not rounded
+    curve_hue = int(curve_hue * 100 + 0.5) / 100;
+
+    bg.setColorAt(0.0, QColor::fromHslF(220 / 360., 0.94, 0.5, 0.4));
+    bg.setColorAt(0.75 / 1.5, QColor::fromHslF(curve_hue / 360., 1.0, 0.68, 0.35));
+    bg.setColorAt(1.0, QColor::fromHslF(curve_hue / 360., 1.0, 0.68, 0.0));
   }
   painter.setBrush(bg);
   painter.drawPolygon(scene.track_vertices);
