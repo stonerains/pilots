05b78b0396d1f982fa73233e2040d22e79750a36
Harald Schäfer <harald.the.engineer@gmail.com>
2024-01-22 17:58:06 +0900

    Los Angeles Model (#31037)
    * 1c888f5b-c213-4c1c-9eba-c587afd047fc/700
    
    * Move to action
    
    * Updates
    
    * Add steer delay
    
    * Update curvature grep
    
    * clip speed
    
    * No car params for now
    
    * Add delay back
    
    * Update
    
    * fix lint
    
    * fix lint
    
    * update model regf
    
    (cherry picked from commit 0067cf3eb1fce18a4d9c548a0c136027c87d43aa)

--- selfdrive/controls/lib/drive_helpers.py
+++ selfdrive/controls/lib/drive_helpers.py
@@ -3,9 +3,7 @@ import math
 from cereal import car, log
 from openpilot.common.conversions import Conversions as CV
 from openpilot.common.numpy_fast import clip, interp
-from openpilot.common.realtime import DT_MDL, DT_CTRL
-from openpilot.selfdrive.modeld.constants import ModelConstants
-from openpilot.selfdrive.controls.ntune import ntune_common_get
+from openpilot.common.realtime import DT_CTRL
 
 # WARNING: this value was determined based on the model's training distribution,
 #          model predictions above this speed can be unpredictable
@@ -171,34 +169,8 @@ def clip_curvature(v_ego, prev_curvature, new_curvature):
   safe_desired_curvature = clip(new_curvature,
                                 prev_curvature - max_curvature_rate * DT_CTRL,
                                 prev_curvature + max_curvature_rate * DT_CTRL)
+
   return safe_desired_curvature
-
-
-def get_lag_adjusted_curvature(steer_delay, v_ego, psis, curvatures):
-  if len(psis) != CONTROL_N:
-    psis = [0.0]*CONTROL_N
-    curvatures = [0.0]*CONTROL_N
-  v_ego = max(MIN_SPEED, v_ego)
-
-  # TODO this needs more thought, use .2s extra for now to estimate other delays
-  steer_delay = ntune_common_get('steerActuatorDelay') + .2
-  path_factor = ntune_common_get('pathFactor')
-
-  # MPC can plan to turn the wheel and turn back before t_delay. This means
-  # in high delay cases some corrections never even get commanded. So just use
-  # psi to calculate a simple linearization of desired curvature
-  current_curvature_desired = curvatures[0]
-  psi = interp(steer_delay, ModelConstants.T_IDXS[:CONTROL_N], psis)
-  average_curvature_desired = psi / (v_ego * steer_delay)
-  desired_curvature = 2 * average_curvature_desired - current_curvature_desired
-
-  # This is the "desired rate of the setpoint" not an actual desired rate
-  max_curvature_rate = MAX_LATERAL_JERK / (v_ego**2) # inexact calculation, check https://github.com/commaai/openpilot/pull/24755
-  safe_desired_curvature = clip(desired_curvature,
-                                current_curvature_desired - max_curvature_rate * DT_MDL,
-                                current_curvature_desired + max_curvature_rate * DT_MDL)
-
-  return safe_desired_curvature * path_factor
 
 
 def get_friction(lateral_accel_error: float, lateral_accel_deadzone: float, friction_threshold: float,
